6275ef6500b81d94b18d5c19232f3e353d9d8f81
Yusuf Ameri <yusuf.americianaki@gmail.com>
2015-12-25 23:19:09 -0500

    Added tagging feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,10 @@ class CommentsController < ApplicationController
 		@comment = Comment.new(comment_params)
 		@comment.article_id = params[:article_id]
 		@comment.save
-		
+
 		redirect_to article_path(@comment.article)
 	end
 	def comment_params
-  params.require(:comment).permit(:author_name, :body)
-end
+  	params.require(:comment).permit(:author_name, :body)
+	end
 end
